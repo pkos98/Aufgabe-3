09d23e42b21b5e341822a59c7f7d8b0f8ec75f69
pkos98 <patrick.kostas@protonmail.com>
2018-04-13 22:13:56 +0200

    Implemented Board & BoardConfig

--- src/games/fatsquirrel/core/Board.java
+++ src/games/fatsquirrel/core/Board.java
@@ -1,4 +1,15 @@
 package games.fatsquirrel.core;
 
 public class Board {
+
+    private BoardConfig config;
+
+    public XY getSize() {
+        return config.getSize();
+    }
+
+    public Board(BoardConfig config) {
+        this.config = config;
+    }
+
 }
